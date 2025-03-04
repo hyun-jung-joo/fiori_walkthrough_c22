08873b218d1d4b1ae950026b3358b1b7bea2ff4f
hyun-jung-joo <wnguswjd0418@naver.com>
2025-03-04 10:22:04 +0900

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,14 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
+    "use strict";
 
-  return Controller.extend("ui5.walkthrough_c22.controller.App", {
-    // App.view.xml 에서 지정해놨던 버튼의 press 이벤트 매소드
-    onShowHello() {
-      alert("Hello World"); // 버튼을 눌렀을 때 발생하는 작동
-    },
-  });
-});
+    return Controller.extend("ui5.walkthrough_c22.controller.App", {
+      // App.view.xml 에서 지정해놨던 버튼의 press 이벤트 매소드
+      onShowHello() {
+        // MessageToast 모듈을 불러와서 사용한다.
+        MessageToast.show("Hello World");
+      },
+    });
+  }
+);
