6fb3b741d34b5da3fa87048b5170b6b65b141401
hyun-jung-joo <wnguswjd0418@naver.com>
2025-03-04 09:54:34 +0900

    Step 4: XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,9 @@
-sap.ui.define(["sap/m/Text"], (Text) => {
+sap.ui.define(["sap/ui/core/mvc/XMLView"], (XMLView) => {
   "use strict";
-  //   alert("UI5 is ready");
-  new Text({
-    text: "Hello World",
-  }).placeAt("content");
+
+  XMLView.create({
+    viewName: "ui5.walkthrough_c22.view.App",
+    // view Name 을 지정해서 view를 불러온다.
+  }).then((oView) => oView.placeAt("content"));
+  // 불러왔으면, index.html의 content부분에 view가 나오게 한다.
 });
